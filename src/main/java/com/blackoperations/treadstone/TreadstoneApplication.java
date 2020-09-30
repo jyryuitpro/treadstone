86259f36103e0ce7d13eabdfda379fae33909bd6
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-09-30 15:26:38 +0900

    Initial commit

---
+++ src/main/java/com/blackoperations/treadstone/TreadstoneApplication.java
@@ -0,0 +1,13 @@
+package com.blackoperations.treadstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreadstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreadstoneApplication.class, args);
+    }
+
+}
