8762f6a46ff436a141f97974a7de7e2c3363ce15
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-01 09:11:33 +0900

    스프링부트 시작 - 03. MockMvc 테스트 만들기
    * SpringBoot 테스트 만들기
    * MockMvc 테스트 만들기

--- src/main/java/com/blackoperations/treadstone/controller/HelloWorldController.java
+++ src/main/java/com/blackoperations/treadstone/controller/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.blackoperations.treadstone.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+//    @PostMapping(value = "/api/helloWorld")
     @GetMapping(value = "/api/helloWorld")
     public String helloWorld() {
         return "HelloWorld";
---
+++ src/test/java/com/blackoperations/treadstone/controller/HelloWorldControllerTest.java
@@ -0,0 +1,41 @@
+package com.blackoperations.treadstone.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+
+    @Autowired
+    private HelloWorldController helloWorldController;
+
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld() {
+//        System.out.println("test");
+        System.out.println(helloWorldController.helloWorld());
+
+        assertThat(helloWorldController.helloWorld()).isEqualTo("HelloWorld");
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/helloWorld"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("HelloWorld"));
+    }
+}
