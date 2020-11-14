5b151e1a9464c83c577d13e3686cecf6c7236b3a
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-14 18:59:46 +0900

    Chapter 08. Mock-Test - 01. Service Test #1

--- src/test/java/com/blackoperations/treadstone/service/PersonServiceTest.java
+++ src/test/java/com/blackoperations/treadstone/service/PersonServiceTest.java
@@ -2,35 +2,38 @@ package com.blackoperations.treadstone.service;
 
 import com.blackoperations.treadstone.domain.Person;
 import com.blackoperations.treadstone.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
-    @Autowired
+    //테스트 대상 클래스
+    @InjectMocks
     private PersonService personService;
 
-    @Autowired
+    @Mock
     private PersonRepository personRepository;
 
     @Test
     void getPeopleByName() {
+        when(personRepository.findByName("martin")).thenReturn(Lists.newArrayList(new Person("martin")));
+
         List<Person> result = personService.getPeopleByName("martin");
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getName()).isEqualTo("martin");
-    }
-
-    @Test
-    void getPerson() {
-        Person person = personService.getPerson(3L);
-
-        assertThat(person.getName()).isEqualTo("dennis");
     }
 }
